c9fb8ff032b2d8a228f40aac020940bce29c6963
Mark <contact.mshin@gmail.com>
2018-12-04 23:28:12 -0700

    created table structure and seed files

---
+++ models/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO customer_accounts (first_name, middle_name, last_name, birthday, phone_number, address, email, password) VALUES 
+("Jamie", "V", "Marquez", "June 5, 1976", 386-253-8381, "1552 Franklin Avenue Daytona Beach, FL 32114", "JamieVMarquez@test.com", "123"),
+("Darren", "T", "Zack", "December 3, 1981", 985-879-8968, "2169 Maryland Avenue Feather Sound, FL 34622", "DarrenTZack@test.com", "456"),
+("Colin", "L", "Reck", "April 1, 1991", 714-578-9292, "4276 Cimmaron Road Fullerton, CA 92632", "ColinLReck@test.com", "789");
+
+INSERT INTO travel_packages (location, flight, date_leaving, date_returning, hotel, car_rental, food_credit, activities) VALUES 
+("Switzerland", "First", "2018-12-25", "2019-01-02", "ARIA Sky Suites", "Convertible", 100, "something expensive"),
+("Canada", "Business", "2019-01-18", "2019-01-23", "The Ritz-Carlton, Kapalua", "Mid-size", 50, "something cool"),
+("Germany", "Economy", "2018-01-15", "2019-01-20", "Blackberry Farm", "Economy", 10, "something free");
+
+INSERT INTO custom_packages (location, flight, date_leaving, date_returning, hotel, car_rental, food_credit, activities) VALUES 
+("United Kingdom", "First", "2019-02-15", "2019-02-20", "Hilton", "Convertible", 100, "something expensive"),
+("Japan", "Business", "2019-02-12", "2019-02-16", "Hyatt", "Mid-size", 50, "something cool"),
+("Spain", "Economy", "2019-01-23", "2019-01-29", "InterContinental", "Economy", 10, "something free");
