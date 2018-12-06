2263261cbe390b80da8e369dd066b1b26704d99d
ohmez <kkid_92@hotmail.com>
2018-12-06 16:36:29 -0700

    merged with mark which had sarah's, updated routes, corrected coupe links on html pages so they work on the server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,54 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all packages
+  app.get("/api/packages", function(req, res) {
+    db.Example.findAll({}).then(function(dbpackages) {
+      res.json(dbpackages);
+    });
+  });
+  app.get("/api/packages/:id", function(req, res) {
+    db.Example.findById(req.params.id).then(function(dbpackages) {
+      res.json(dbpackages);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/api/packages", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  app.delete("/api/packages/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.json(dbExample);
+    });
+  });
+
+  // start of customer api routes
+
+  app.get("/api/customers", function(req, res) {
+    db.Example.findAll({}).then(function(dbpackages) {
+      res.json(dbpackages);
+    });
+  });
+  app.get("/api/customers/:id", function(req, res) {
+    db.Example.findById(req.params.id).then(function(dbpackages) {
+      res.json(dbpackages);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/customers", function(req, res) {
+    db.Example.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/customers/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
