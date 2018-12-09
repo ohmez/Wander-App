b4d0b30c55265d25b684a8112994d87279f49897
Mark <contact.mshin@gmail.com>
2018-12-09 02:07:13 -0700

    file changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,38 @@
 var db = require("../models");
 var path = require("path");
+
 module.exports = function(app) {
-  // Load index page
+  // Load Index Page
   app.get("/", function(req, res) {
-    // res.sendFile(path.join(__dirname, "../html5up-spectral/index.html"));
     res.render("index", {
-      layout: "home"
+      layout: "home",
+      title: "Packages"
     });
   });
+  // Load Prebuilt Packages Page
   app.get("/packages", function(req, res) {
-    // res.sendFile(path.join(__dirname, "../html5up-spectral/packages.html"));
     res.render("packages", {
-      layout: "main"
+      layout: "main",
+      title: "Packages"
     });
   });
+  // Load Custom Page Form Page
   app.get("/packages/custom-package", function(req, res) {
-    // res.sendFile(path.join(__dirname, "../html5up-spectral/packages.html"));
-    res.render("custompackage", { layout: "main" });
+    res.render("custom-package", {
+      layout: "main",
+      title: "Custom Package"
+    });
   });
+  // Load Individual Package Info Page
   app.get("/booking", function(req, res) {
     res.sendFile(path.join(__dirname, "../html5up-spectral/booking.html"));
   });
-
+  // Load Account Sign Up Page
   app.get("/signup", function(req, res) {
-    res.render("customersignup", { layout: "main" });
+    res.render("customer-signup", {
+      layout: "main",
+      title: "Sign Up"
+    });
   });
 
   // Load example page and pass in an example by id
