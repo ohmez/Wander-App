822c968568fdba99f4a279d633d748b319d6e7f5
Red Fox <sarahanne118@gmail.com>
2018-12-13 19:23:00 -0700

    update schema

--- models/schema.sql
+++ models/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE customer_accounts (
     middleName VARCHAR (100) NOT NULL,
     lastName VARCHAR (100) NOT NULL,
     birthday VARCHAR (320) NOT NULL,
-	phoneNumber VARCHAR NOT NULL,
+	phoneNumber VARCHAR (13) NOT NULL,
 	address VARCHAR (320) NOT NULL,
 	email VARCHAR (320) NOT NULL,
 	password VARCHAR (320) NOT NULL,
@@ -50,10 +50,15 @@ INSERT INTO customer_accounts (firstName, middleName, lastName, birthday, phoneN
 ("Colin", "L", "Reck", 01/04/1991, 714-578-9292, "4276 Cimmaron Road Fullerton, CA 92632", "ColinLReck@test.com", "789");
 
 INSERT INTO travel_packages (category, location, flight, dateLeaving, dateReturning, hotel, carRental, foodCredit, activities) VALUES 
-("Thrifty", "Switzerland", "First", "2018-12-25", "2019-01-02", "ARIA Sky Suites", "Convertible", 100, "something expensive"),
-("Comfort", "Canada", "Business", "2019-01-18", "2019-01-23", "The Ritz-Carlton, Kapalua", "Mid-size", 50, "something cool"),
-("Bliss", "Germany", "Economy", "2018-01-15", "2019-01-20", "Blackberry Farm", "Economy", 10, "something free"),
-("Bliss", "Canada", "Business", "2019-01-18", "2019-01-23", "The Ritz-Carlton, Kapalua", "Mid-size", 50, "something cool");
+("Thrifty", "Switzerland", "Economy", "2018-12-25", "2019-01-02", "Balmers Hostel", "Bus Pass", 20, "Grindelwald Hike"),
+("Thrifty", "Spain", "Economy", "2019-08-15", "2019-08-25", "Hostel One Paralelo", "Bus Pass", 40, "Saunter up La Rambla"),
+("Thrifty", "Scotland", "Economy", "2019-01-02", "2019-01-10", "Royal Mile Backpackers", "Bus Pass", 20, "Highlands Hike"),
+("Comfort", "Canada", "Business", "2019-01-18", "2019-01-23", "Le Westin Montreal", "Mid-size", 50, "Montreal Walking Tour"),
+("Comfort", "France", "Business", "2019-02-18", "2019-02-23", "Paris Marriott Champs Elysees", "Metro Card", 50, "Paris Food Tour"),
+("Comfort", "Japan", "Business", "2019-01-02", "2019-01-08", "Little Tokyo Hotel", "Metro Card", 50, "Sushi Making"),
+("Bliss", "Germany", "First", "2019-09-15", "2019-09-30", "Blackberry Farm", "Chauffeur", 110, "Oktoberfest"),
+("Bliss", "Canada", "First", "2019-01-18", "2019-01-23", "Hotel Place d'Armes", "Chauffeur", 150, "Food Tour"),
+("Bliss", "New York", "First", "2019-01-18", "2019-01-23", "The Ritz-Carlton", "Chauffeur", 250, "Russian Tea House");
 
 INSERT INTO custom_packages (location, flight, dateLeaving, dateReturning, hotel, carRental, foodCredit, activities) VALUES 
 ("United Kingdom", "First", "2019-02-15", "2019-02-20", "Hilton", "Convertible", 100, "something expensive"),
